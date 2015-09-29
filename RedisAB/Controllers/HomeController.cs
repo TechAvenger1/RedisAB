dc615d38e04c2530710a48a9619422c8ad2c12ad
TechAvenger <techavenger.vzw@gmail.com>
2015-09-29 02:38:27 -0700

    Testing

--- RedisAB/Controllers/HomeController.cs
+++ RedisAB/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace RedisAB.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description.";
 
             return View();
         }
